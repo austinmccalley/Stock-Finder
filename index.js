c85ccfb4ea98fb7062e7f5226b6183d0d5043f4d
Austin M <austin@austinmccalley>
2019-11-23 12:36:51 -0800

    Updated to 1.0.2 -- Added curl backup

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 var rp = require('request-promise-native');
 
-module.exports = function (symbol, apiKey) {
+module.exports = function (symbol, apiKey, curl=false) {
     return new Promise(function (fulfill, reject) {
         if (apiKey == undefined || apiKey == null) {
             reject({
                 error: 'Please provide a proper IEX Cloud api key.'
             })
         } else {
-            rp(`https://cloud.iexapis.com/stable/tops?token=${apiKey}&symbols=${symbol}`)
+            if (curl) {
+               rp(`https://cloud.iexapis.com/stable/stock/${symbol}/quote?token=${apiKey}`)
                 .then(function (res_) {
                     if (!res_) reject({
-                        error: "Something went wrong. Please check "
+                        error: "Something went wrong."
                     })
                     fulfill(JSON.parse(res_));
                 })
                 .catch(function (err) {
                     reject(err);
-                });
-
-        }
+                });        
+            }else{
+                rp(`https://cloud.iexapis.com/stable/tops?token=${apiKey}&symbols=${symbol}`)
+                    .then(function (res_) {
+                        if (!res_) reject({
+                            error: "Something went wrong."
+                        })
+                        fulfill(JSON.parse(res_));
+                    })
+                    .catch(function (err) {
+                        reject(err);
+                    });
+                }
+            }
+            
     });
 }
