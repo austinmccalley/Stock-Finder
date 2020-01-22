cd00cdbf16efbb8ffdf40050f9753a0cceb60e63
Austin McCalley <austin@austinmccalley.com>
2020-01-22 12:02:54 -0800

    Working on issue #10 and #7

--- index.js
+++ index.js
@@ -6,22 +6,27 @@ const sf = class StockFinder {
     this.tickers = tickers;
     this.apiKey = apiKey;
     this.curl = curl;
+    this.error = false;
   }
 
   getStock() {
     return new Promise((resolve, reject) => {
       const url = `https://cloud.iexapis.com/${this.version}/tops?token=${this.apiKey}&symbols=${this.tickers}`;
       if (this.apikey === undefined) {
-        rp(url).then((res) => {
+        rp(url, {
+          resolveWithFullResponse: true,
+          simple: true,
+        }).then((res) => {
           if (!res) {
             reject(new Error(
               'Something went wrong here.',
             ));
           } else {
-            resolve(JSON.parse(res));
+            resolve(JSON.parse(res.body));
           }
         }).catch((err) => {
-          reject(err);
+          this.assignError(err);
+          reject(this.error);
         });
       } else {
         reject(Error({
@@ -37,16 +42,20 @@ const sf = class StockFinder {
         const stocks = Array.prototype.toString(this.tickers);
         const url = `https://cloud.iexapis.com/${this.version}/tops?token=${this.apiKey}&symbols=${stocks}`;
         if (this.apiKey === undefined) {
-          rp(url).then((res) => {
+          rp(url, {
+            resolveWithFullResponse: true,
+            simple: true,
+          }).then((res) => {
             if (!res) {
               reject(new Error(
                 'Something went wrong here.',
               ));
             } else {
-              resolve(JSON.parse(res));
+              resolve(JSON.parse(res.body));
             }
           }).catch((err) => {
-            reject(err);
+            this.assignError(err);
+            reject(this.error);
           });
         }
       } else {
@@ -57,6 +66,20 @@ const sf = class StockFinder {
     });
   }
 
+  assignError(err) {
+    const statusCodes = [400, 401, 402, 403, 404, 413, 429, 451, 500];
+
+    if (statusCodes.includes(err.statusCode)) {
+      this.error = {
+        name: err.name,
+        statusCode: err.statusCode,
+        message: err.message,
+        error: err.error,
+      };
+    } else {
+      throw err;
+    }
+  }
 };
 
 module.exports = sf;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 const chai = require('chai');
 
-const { expect } = chai;
+const {
+  expect
+} = chai;
 
 const chaiAsPromised = require('chai-as-promised');
 
@@ -8,19 +10,36 @@ chai.use(chaiAsPromised);
 
 require('dotenv').config();
 
-const stockFinder = require('../index.js');
+const StockFinder = require('../index.js');
 
+describe('StockFinder Return Stocks off of stable release.', () => {
+  it('Should return TSLA stock', () => {
+    const sf = new StockFinder('stable', 'TSLA', process.env.API_KEY, false);
+    const res = sf.getStock();
+    return expect(res).to.eventually.have.length(1);
+    done();
+  });
 
-it('Should return TSLA stock', () => {
-  const sf = new stockFinder('stable', 'TSLA', process.env.API_KEY, false);
-  const res = sf.getStock();
-  return expect(res).to.eventually.have.length(1);
-  done();
+  it('Should return multiple stocks', () => {
+    const sf = new StockFinder('stable', ['TSLA', 'AAPL'], process.env.API_KEY, false);
+    const res = sf.getStock();
+    return expect(res).to.eventually.have.length(2);
+    done();
+  });
 });
 
-it('Should return multiple stocks', () => {
-  const sf = new stockFinder('stable', ['TSLA', 'AAPL'], process.env.API_KEY, false);
-  const res = sf.getStock();
-  return expect(res).to.eventually.have.length(2);
-  done();
-});
+describe('StockFinder return stocks off of the beta release', () => {
+  it('Should return $TSLA', () => {
+    const sf = new StockFinder('beta', 'TSLA', process.env.API_KEY, false);
+    const res = sf.getStock();
+    return expect(res).to.eventually.have.length(1);
+    done();
+  });
+
+  it('Should return $TSLA and $AAPL', () => {
+    const sf = new StockFinder('beta', ['TSLA', 'AAPL'], process.env.API_KEY, false);
+    const res = sf.getStock();
+    return expect(res).to.eventually.have.length(2);
+    done();
+  });
+})
